68706b027b48559bf2249a0c0a8f19b854280eb3
CamiloDP96 <cadp.96@outlook.com>
2023-09-26 20:20:09 -0500

    options menu

--- src/main/java/com/mystore/App.java
+++ src/main/java/com/mystore/App.java
@@ -11,26 +11,100 @@ public class App
     public static void main( String[] args )
     {
         Scanner sc = new Scanner(System.in);
-        String product;//nombre del producto
-        short purchaseQuantity;// cantidad ordenada / dia , mes
-        int inventoryNumber;//numero de inventario
-        int invetoryQuantity;//cantidad en inventario
-        double productPrice;//precio de producto
-
-        //Creacion de objeto
-        product = "catfood";
-        System.out.println("Por favor ingrese la cantidad ordenada:");
-        purchaseQuantity = sc.nextShort();
-        inventoryNumber=45444823;
-        invetoryQuantity = 777888;
-        invetoryQuantity = invetoryQuantity - purchaseQuantity;
-        productPrice = 789543.99;
-
-        //Uso de las variables en impresion
-        System.out.println("producto | cantidad | codigo de registro | cantidad en inventario| precio");
-        System.out.println(product + " | " + purchaseQuantity + " | " + inventoryNumber + " | " + invetoryQuantity + " | " + productPrice);
+        displayMenu(sc);
         if(sc!=null){
             sc.close();
+            }
         }
-    }
+        public static void displayMenu(Scanner sc){
+            int choise;
+            System.out.println(
+            """
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢲⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄⠂⢉⠤⠐⠋⠈⠡⡈⠉⠐⠠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⢀⡀⢠⣤⠔⠁⢀⠀⠀⠀⠀⠀⠀⠀⠈⢢⠀⠀⠈⠱⡤⣤⠄⣀⠀⠀⠀⠀⠀
+            ⠀⠀⠰⠁⠀⣰⣿⠃⠀⢠⠃⢸⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠈⢞⣦⡀⠈⡇⠀⠀⠀
+            ⠀⠀⠀⢇⣠⡿⠁⠀⢀⡃⠀⣈⠀⠀⠀⠀⢰⡀⠀⠀⠀⠀⢢⠰⠀⠀⢺⣧⢰⠀⠀⠀⠀
+            ⠀⠀⠀⠈⣿⠁⡘⠀⡌⡇⠀⡿⠸⠀⠀⠀⠈⡕⡄⠀⠐⡀⠈⠀⢃⠀⠀⠾⠇⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠇⡇⠃⢠⠀⠶⡀⡇⢃⠡⡀⠀⠀⠡⠈⢂⡀⢁⠀⡁⠸⠀⡆⠘⡀⠀⠀⠀⠀
+            ⠀⠀⠀⠸⠀⢸⠀⠘⡜⠀⣑⢴⣀⠑⠯⡂⠄⣀⣣⢀⣈⢺⡜⢣⠀⡆⡇⠀⢣⠀⠀⠀⠀
+            ⠀⠀⠀⠇⠀⢸⠀⡗⣰⡿⡻⠿⡳⡅⠀⠀⠀⠀⠈⡵⠿⠿⡻⣷⡡⡇⡇⠀⢸⣇⠀⠀⠀
+            ⠀⠀⢰⠀⠀⡆⡄⣧⡏⠸⢠⢲⢸⠁⠀⠀⠀⠀⠐⢙⢰⠂⢡⠘⣇⡇⠃⠀⠀⢹⡄⠀⠀
+            ⠀⠀⠟⠀⠀⢰⢁⡇⠇⠰⣀⢁⡜⠀⠀⠀⠀⠀⠀⠘⣀⣁⠌⠀⠃⠰⠀⠀⠀⠈⠰⠀⠀
+            ⠀⡘⠀⠀⠀⠀⢊⣤⠀⠀⠤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠄⠀⢸⠃⠀⠀⠀⠀⠀⠃⠀
+            ⢠⠁⢀⠀⠀⠀⠈⢿⡀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⢀⠏⠀⠀⠀⠀⠀⠀⠸⠀
+            ⠘⠸⠘⡀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠁⠀⠃⠀⠀⠀⠀⢀⠎⠀⠀⠀⠀⠀⢠⠀⠀⡇
+            ⠀⠇⢆⢃⠀⠀⠀⠀⠀⡏⢲⢤⢀⡀⠀⠀⠀⠀⠀⢀⣠⠄⡚⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀
+            ⢰⠈⢌⢎⢆⠀⠀⠀⠀⠁⣌⠆⡰⡁⠉⠉⠀⠉⠁⡱⡘⡼⠇⠀⠀⠀⠀⢀⢬⠃⢠⠀⡆
+            ⠀⢢⠀⠑⢵⣧⡀⠀⠀⡿⠳⠂⠉⠀⠀⠀⠀⠀⠀⠀⠁⢺⡀⠀⠀⢀⢠⣮⠃⢀⠆⡰⠀
+            ⠀⠀⠑⠄⣀⠙⡭⠢⢀⡀⠀⠁⠄⣀⣀⠀⢀⣀⣀⣀⡠⠂⢃⡀⠔⠱⡞⢁⠄⣁⠔⠁⠀
+            ⠀⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠉⠁⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇"""
+            );
+            System.out.println("___________________________________________\n"+"Administrador Mi Tienda\n" + "___________________________________________\n" + "1. Agregar a inventario\n" + "2. Remover de inventario\n" + "3. actualizar producto de inventario\n" + "4. Eliminar producto de inventario\n" + "5. Ver inventario\n" + "6. Cerrar menu");
+            choise = sc.nextInt();
+            switch (choise) {
+                case 1:
+                    addProduct();
+                    break;
+                case 2:
+                    removeProduct();
+                    break;
+                case 3:
+                    updateProduct();
+                    break;
+                case 4:
+                    deleteProduct();
+                    break;
+                case 5:
+                    printInventory();
+                    break;
+                case 6:
+                    killProgram();
+                    break;
+                default:
+                    System.out.println("opcion no valida. por favor intente de nuevo");
+                    break;
+            }
+        }
+
+        static void addProduct(){
+            System.out.println("add Product");
+            return;
+        }
+        static void removeProduct(){
+            System.out.println("remove product");
+            return;
+        }
+        static void updateProduct(){
+            System.out.println("update product");
+            return;
+        }
+        static void deleteProduct(){
+            System.out.println("delete product");
+            return;
+        }
+        static void printInventory(){
+            System.out.println("Inventario");
+            String product;//nombre del producto
+            int inventoryNumber;//numero de inventario
+            int invetoryQuantity;//cantidad en inventario
+            double productPrice;//precio de producto
+
+            //Creacion de objeto
+            product = "catfood";
+            System.out.println("Por favor ingrese la cantidad ordenada:");
+            inventoryNumber=45444823;
+            invetoryQuantity = 777888;
+            productPrice = 789543.99;
+            //Uso de las variables en impresion
+            System.out.println("producto | cantidad | codigo de registro | cantidad en inventario| precio");
+            System.out.println(product + " | "  + inventoryNumber + " | " + invetoryQuantity + " | " + productPrice);
+            return;
+        }
+        static void killProgram(){
+            System.out.println("exit");
+        }
 }
+
